533c36d2f9b144d1d6deba389710c70fad6eedf3
Jason5Lee <jason5lee@hotmail.com>
2022-06-18 21:54:35 +0800

    remove old template source

--- src/templates/git_ignore.rs
+++
@@ -1,9 +0,0 @@
-use super::TemplateFile;
-
-pub(super) const GIT_IGNORE: TemplateFile = TemplateFile {
-    path: |_| ".gitignore".into(),
-    write_content: |_, w| {
-        write!(w, r"
-")
-    },
-};
--- src/templates/gitattributes.rs
+++
@@ -1,8 +0,0 @@
-use super::TemplateFile;
-
-pub(super) const GITATTRIBUTES: TemplateFile = TemplateFile {
-    path: |_| ".gitattributes".into(),
-    write_content: |_, w| {
-        writeln!(w, r"*.bat text eol=crlf")
-    },
-};
