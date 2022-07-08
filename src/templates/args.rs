9eb0c172a61611cb53dee1b21f9c9b6275539ae5
Jason5Lee <jason5lee@hotmail.com>
2022-07-08 18:40:36 +0800

    support iterative mode

--- src/templates/args.rs
+++ src/templates/args.rs
@@ -3,8 +3,89 @@ mod default_package;
 use super::template_file::Args;
 use crate::{log_error, Logged};
 use fxhash::FxHashMap;
+use std::io::{BufRead, Write};
+
+const GROUP: &str = "group";
+const GROUP_DESCRIPTION: &str = "Project group ID";
+const ARTIFACT: &str = "artifact";
+const ARTIFACT_DESCRIPTION: &str = "Project artifact ID";
+const PACKAGE: &str = "package";
+const PACKAGE_DESCRIPTION: &str = "Project root package";
+const PACKAGE_DEFAULT_DESCRIPTION: &str =
+    "If not specified, use <group>.<artifact> and try to fix the illegal part";
+const VERSION: &str = "version";
+const VERSION_DESCRIPTION: &str = "Project version";
+const DEFAULT_VERSION: &str = "1.0.0-SNAPSHOT";
+const TARGET_JVM: &str = "targetJvm";
+const TARGET_JVM_DESCRIPTION: &str = "Target JVM version";
+
+fn cli_read_arg(prompt: std::fmt::Arguments, default: Option<String>) -> std::io::Result<String> {
+    let mut stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout().lock();
+    let mut line = String::new();
+    loop {
+        write!(stdout, "{}", prompt)?;
+        if let Some(d) = &default {
+            write!(stdout, " ({d})")?;
+        }
+        write!(stdout, ":")?;
+        stdout.flush()?;
+        line.clear();
+        stdin.read_line(&mut line)?;
+        let value = line.trim();
+        if value.is_empty() {
+            if let Some(d) = default {
+                return Ok(d);
+            }
+        } else {
+            return Ok(value.to_string());
+        }
+    }
+}
+fn read_arg(
+    arg: &str,
+    description: &str,
+    args: &mut FxHashMap<String, String>,
+    default: Option<String>,
+    iterative: bool,
+) -> Result<(), Logged> {
+    let arg_ref = args.entry(arg.to_string()).or_insert(String::new());
+
+    let candidate_value = if arg_ref.is_empty() {
+        default
+    } else {
+        Some(std::mem::take(arg_ref))
+    };
+    let value = if iterative {
+        cli_read_arg(format_args!("Enter {description}"), candidate_value)
+            .map_err(|err| log_error(format_args!("{err}")))
+    } else {
+        candidate_value.ok_or_else(|| log_error(format_args!("missing argument `{}`", arg)))
+    };
+
+    *arg_ref = value?;
+    Ok(())
+}
+fn read_package(args: &mut FxHashMap<String, String>, iterative: bool) -> Result<(), Logged> {
+    let candidate_value = if let Some(exists) = args.get_mut("package") {
+        Ok(std::mem::take(exists))
+    } else {
+        default_package::get_package(&args["group"], &args["artifact"])
+    };
+
+    let value = if iterative {
+        cli_read_arg(format_args!("Enter Package"), candidate_value.ok())
+            .map_err(|err| log_error(format_args!("{err}")))
+    } else {
+        candidate_value.map_err(|err| log_error(format_args!("{err}")))
+    };
+
+    args.insert("package".to_string(), value?);
+    Ok(())
+}
 
 pub(super) fn get_args(
+    iterative: bool,
     cmdline_args_value: Vec<(String, String)>,
     args_info: Args,
 ) -> Result<FxHashMap<String, String>, Logged> {
@@ -19,58 +100,52 @@ pub(super) fn get_args(
         result.insert(k, v);
     }
 
-    let group = result
-        .get("group")
-        .ok_or_else(|| log_error(format_args!("missing argument: group")))?;
-    let artifact = result
-        .get("artifact")
-        .ok_or_else(|| log_error(format_args!("missing argument: artifact")))?;
-    result
-        .get("version")
-        .ok_or_else(|| log_error(format_args!("missing argument: version")))?;
-    let pkg_info = if let Some(package) = result.get("package") {
-        default_package::analysis_package(package)?
-    } else {
-        default_package::get_package(&group, &artifact)?
-    };
-    result.insert("package".to_string(), pkg_info.package);
-    result.insert("packagePath".to_string(), pkg_info.package_path);
+    read_arg("group", "Group ID", &mut result, None, iterative)?;
+    read_arg("artifact", "Artifact ID", &mut result, None, iterative)?;
+    read_arg("version", "Version", &mut result, None, iterative)?;
+
+    read_package(&mut result, iterative)?;
+    result.insert(
+        "packagePath".to_string(),
+        result["package"].replace('.', "/"),
+    );
 
     if let Some(target_jvm_info) = target_jvm {
-        if let Some(target_jvm) = result.get("targetJvm") {
-            let target_jvm_java = target_jvm.replace('.', "_");
-            result.insert("targetJvmJava".to_string(), target_jvm_java);
-        } else {
-            if let Some(default) = target_jvm_info.default {
-                let java = default.replace('.', "_");
-                result.insert("targetJvm".to_string(), default);
-                result.insert("targetJvmJava".to_string(), java);
-            } else {
-                return Err(log_error(format_args!("missing argument: targetJvm")));
-            }
-        }
+        read_arg(
+            "targetJvm",
+            "Target JVM",
+            &mut result,
+            target_jvm_info.default,
+            iterative,
+        )?;
+        result.insert(
+            "targetJvmJava".to_string(),
+            result["targetJvm"].replace('.', "_"),
+        );
     }
 
     for (arg_name, arg_info) in args {
-        if !result.contains_key(&arg_name) {
-            if let Some(default) = arg_info.default {
-                result.insert(arg_name, default);
-            } else {
-                return Err(log_error(format_args!("missing argument: {}", arg_name)));
-            }
-        }
+        read_arg(
+            &arg_name,
+            &arg_info.description,
+            &mut result,
+            arg_info.default,
+            iterative,
+        )?;
     }
     result.insert("dollar".to_string(), "$".to_string());
     Ok(result)
 }
 
 pub(super) fn print_args_list(args_info: Args) {
-    println!("    group: Project groupId
-    artifact: Project artifactId
-    package: Project root package. If not specified, use <group>.<artifact> and try to fix the illegal part
-    version: Project version");
+    println!(
+        "    {GROUP}: {GROUP_DESCRIPTION}
+    {ARTIFACT}: {ARTIFACT_DESCRIPTION}
+    {PACKAGE}: {PACKAGE_DESCRIPTION}. {PACKAGE_DEFAULT_DESCRIPTION}
+    {VERSION}: {VERSION_DESCRIPTION}, the default is {DEFAULT_VERSION}"
+    );
     if let Some(target_jvm) = args_info.target_jvm {
-        print!("    targetJvm: Target JVM version");
+        print!("    {TARGET_JVM}: {TARGET_JVM_DESCRIPTION}");
         if let Some(default) = target_jvm.default {
             print!(", the default is {}", default)
         }
--- src/templates/args/default_package.rs
+++ src/templates/args/default_package.rs
@@ -1,57 +1,27 @@
-use crate::{log_error, Logged};
-
 const JAVA_KEYWORDS: phf::Set<&'static str> = phf::phf_set! {"abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"};
 const KOTLIN_HARD_KEYWORDS: phf::Set<&'static str> = phf::phf_set! {"as", "break", "class", "continue", "do", "else", "false", "for", "fun", "if", "in", "interface", "is", "null", "object", "package", "return", "super", "this", "throw", "true", "try", "typealias", "typeof", "val", "var", "when", "while"};
 
 const PACKAGE_ERR_PREFIX: &str = "default package name invalid and cannot be fixed";
-const PACKAGE_ERR_SUFFIX: &str = "please assign the package";
+const PACKAGE_ERR_SUFFIX: &str = "please manually define the package";
 
-#[derive(Default)]
-pub(super) struct PackageInfo {
-    pub(super) package: String,
-    pub(super) package_path: String,
-}
-
-pub(super) fn analysis_package(package: &str) -> Result<PackageInfo, Logged> {
-    let mut package_info = PackageInfo::default();
-    let mut first = true;
-
-    for item in package.split('.') {
-        let item = fix_package_item(item)?;
-        if !first {
-            package_info.package.push('.');
-            package_info.package_path.push('/');
-        }
-        first = false;
-
-        package_info.package.push_str(&item);
-        package_info.package_path.push_str(&item);
-    }
-    Ok(package_info)
-}
-
-pub(super) fn get_package(group: &str, artifact: &str) -> Result<PackageInfo, Logged> {
-    let mut package_info = PackageInfo::default();
+pub(super) fn get_package(group: &str, artifact: &str) -> Result<String, String> {
+    let mut result = String::new();
 
     for group_path in group.split('.') {
         let item = fix_package_item(group_path)?;
 
-        package_info.package.push_str(&item);
-        package_info.package.push('.');
-
-        package_info.package_path.push_str(&item);
-        package_info.package_path.push('/');
+        result.push_str(&item);
+        result.push('.');
     }
     let item = fix_package_item(artifact)?;
-    package_info.package.push_str(&item);
-    package_info.package_path.push_str(&item);
-    Ok(package_info)
+    result.push_str(&item);
+    Ok(result)
 }
 
 // Fix the package item https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html
 // It assumes the `item` is either extracted from a valid group or a valid name.
 // Which means it should not be empty.
-fn fix_package_item(item: &str) -> Result<String, Logged> {
+fn fix_package_item(item: &str) -> Result<String, String> {
     // Note: we can actually use kotlin keyword in package,
     // but in the code it need to be wrapped in ``.
     // This makes the logic very complicated so we just change it.
@@ -72,10 +42,10 @@ fn fix_package_item(item: &str) -> Result<String, Logged> {
             result.push(fix_non_numeric_char(first)?);
         }
     } else {
-        return Err(log_error(format_args!(
-            "{}, empty item found in package, {}",
+        return Err(format!(
+            "{}, empty item found, {}",
             PACKAGE_ERR_PREFIX, PACKAGE_ERR_SUFFIX
-        )));
+        ));
     };
 
     for ch in item_chars {
@@ -89,15 +59,15 @@ fn fix_package_item(item: &str) -> Result<String, Logged> {
     Ok(result)
 }
 
-fn fix_non_numeric_char(ch: char) -> Result<char, Logged> {
+fn fix_non_numeric_char(ch: char) -> Result<char, String> {
     if ch.is_alphabetic() {
         Ok(ch.to_ascii_lowercase())
     } else if ch == '-' {
         Ok('_')
     } else {
-        Err(log_error(format_args!(
-            "{}, invalid char `{}` found in package, {}",
+        Err(format!(
+            "{}, invalid char `{}` found, {}",
             PACKAGE_ERR_PREFIX, ch, PACKAGE_ERR_SUFFIX
-        )))
+        ))
     }
 }
